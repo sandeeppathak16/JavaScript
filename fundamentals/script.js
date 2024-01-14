bae13aa15925183480fee2b7d22504c3568a553a
sandeeppathak16 <spathak1698@gmail.com>
2024-01-14 12:20:24 +0530

    use ternary operator

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -32,3 +32,10 @@ if ((dolphinsAvgScore > koalasAvgScore) && (dolphinsAvgScore > 100)){
 }
 
 console.log(`result: ${winner}`);
+
+const bill = 275
+
+const tip = ((bill < 300) && (bill > 50 )) ? bill * (15 / 100) : bill * (20 / 100)
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
+
