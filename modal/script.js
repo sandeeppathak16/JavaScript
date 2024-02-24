e9b9d9f6333153190dae74c24de854b97c99bbe3
sandeeppathak16 <spathak1698@gmail.com>
2024-02-24 14:17:57 +0530

    close modal on escabe

--- modal/script.js
+++ modal/script.js
@@ -5,17 +5,32 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i=0; i < btnOpenModal.length; i++){
-    btnOpenModal[i].addEventListener('click', function () {
-        model.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    })
-};
+
+const openModal = function () {
+    model.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
 
 const closeModal = function () {
     model.classList.add('hidden');
     overlay.classList.add('hidden');
 }
 
+
+for (let i=0; i < btnOpenModal.length; i++){
+    btnOpenModal[i].addEventListener('click', openModal)
+};
+
+
 btnCloseModal.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape'){
+        if (!model.classList.contains('hidden')){
+            closeModal()
+        }
+    }
+});
