df24306fc5a8395450f265ccb976fe649126078a
sandeeppathak16 <spathak1698@gmail.com>
2024-08-27 16:02:24 +0530

    objects, set, map, arrays

--- data+structures+modern+operator+and+string/script.js
+++ data+structures+modern+operator+and+string/script.js
@@ -2,17 +2,26 @@
 
 const books = [
   {
-    title: 'Algorithms',
-    author: ['Robert Sedgewick', 'Kevin Wayne'],
-    publisher: 'Addison-Wesley Professional',
-    publicationDate: '2011-03-24',
+    title: "Algorithms",
+    author: ["Robert Sedgewick", "Kevin Wayne"],
+    publisher: "Addison-Wesley Professional",
+    publicationDate: "2011-03-24",
     edition: 4,
-    keywords: ['computer science', 'programming', 'algorithms', 'data structures', 'java', 'math', 'software', 'engineering'],
+    keywords: [
+      "computer science",
+      "programming",
+      "algorithms",
+      "data structures",
+      "java",
+      "math",
+      "software",
+      "engineering",
+    ],
     pages: 976,
-    format: 'hardcover',
-    ISBN: '9780321573513',
-    language: 'English',
-    programmingLanguage: 'Java',
+    format: "hardcover",
+    ISBN: "9780321573513",
+    language: "English",
+    programmingLanguage: "Java",
     onlineContent: true,
     thirdParty: {
       goodreads: {
@@ -20,23 +29,33 @@ const books = [
         ratingsCount: 1733,
         reviewsCount: 63,
         fiveStarRatingCount: 976,
-        oneStarRatingCount: 13
-      }
+        oneStarRatingCount: 13,
+      },
     },
-    highlighted: true
+    highlighted: true,
   },
   {
-    title: 'Structure and Interpretation of Computer Programs',
-    author: ['Harold Abelson', 'Gerald Jay Sussman', 'Julie Sussman (Contributor)'],
-    publisher: 'The MIT Press',
-    publicationDate: '2022-04-12',
+    title: "Structure and Interpretation of Computer Programs",
+    author: [
+      "Harold Abelson",
+      "Gerald Jay Sussman",
+      "Julie Sussman (Contributor)",
+    ],
+    publisher: "The MIT Press",
+    publicationDate: "2022-04-12",
     edition: 2,
-    keywords: ['computer science', 'programming', 'javascript', 'software', 'engineering'],
+    keywords: [
+      "computer science",
+      "programming",
+      "javascript",
+      "software",
+      "engineering",
+    ],
     pages: 640,
-    format: 'paperback',
-    ISBN: '9780262543231',
-    language: 'English',
-    programmingLanguage: 'JavaScript',
+    format: "paperback",
+    ISBN: "9780262543231",
+    language: "English",
+    programmingLanguage: "JavaScript",
     onlineContent: false,
     thirdParty: {
       goodreads: {
@@ -44,23 +63,30 @@ const books = [
         ratingsCount: 14,
         reviewsCount: 3,
         fiveStarRatingCount: 8,
-        oneStarRatingCount: 0
-      }
+        oneStarRatingCount: 0,
+      },
     },
-    highlighted: true
+    highlighted: true,
   },
   {
-    title: 'Computer Systems: A Programmer\'s Perspective',
-    author: ['Randal E. Bryant', 'David Richard O\'Hallaron'],
-    publisher: 'Prentice Hall',
-    publicationDate: '2002-01-01',
+    title: "Computer Systems: A Programmer's Perspective",
+    author: ["Randal E. Bryant", "David Richard O'Hallaron"],
+    publisher: "Prentice Hall",
+    publicationDate: "2002-01-01",
     edition: 1,
-    keywords: ['computer science', 'computer systems', 'programming', 'software', 'C', 'engineering'],
+    keywords: [
+      "computer science",
+      "computer systems",
+      "programming",
+      "software",
+      "C",
+      "engineering",
+    ],
     pages: 978,
-    format: 'hardcover',
-    ISBN: '9780130340740',
-    language: 'English',
-    programmingLanguage: 'C',
+    format: "hardcover",
+    ISBN: "9780130340740",
+    language: "English",
+    programmingLanguage: "C",
     onlineContent: false,
     thirdParty: {
       goodreads: {
@@ -68,23 +94,31 @@ const books = [
         ratingsCount: 1010,
         reviewsCount: 57,
         fiveStarRatingCount: 638,
-        oneStarRatingCount: 16
-      }
+        oneStarRatingCount: 16,
+      },
     },
-    highlighted: true
+    highlighted: true,
   },
   {
-    title: 'Operating System Concepts',
-    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
-    publisher: 'John Wiley & Sons',
-    publicationDate: '2004-12-14',
+    title: "Operating System Concepts",
+    author: ["Abraham Silberschatz", "Peter B. Galvin", "Greg Gagne"],
+    publisher: "John Wiley & Sons",
+    publicationDate: "2004-12-14",
     edition: 10,
-    keywords: ['computer science', 'operating systems', 'programming', 'software', 'C', 'Java', 'engineering'],
+    keywords: [
+      "computer science",
+      "operating systems",
+      "programming",
+      "software",
+      "C",
+      "Java",
+      "engineering",
+    ],
     pages: 921,
-    format: 'hardcover',
-    ISBN: '9780471694663',
-    language: 'English',
-    programmingLanguage: 'C, Java',
+    format: "hardcover",
+    ISBN: "9780471694663",
+    language: "English",
+    programmingLanguage: "C, Java",
     onlineContent: false,
     thirdParty: {
       goodreads: {
@@ -92,21 +126,21 @@ const books = [
         ratingsCount: 2131,
         reviewsCount: 114,
         fiveStarRatingCount: 728,
-        oneStarRatingCount: 65
-      }
-    }
+        oneStarRatingCount: 65,
+      },
+    },
   },
   {
-    title: 'Engineering Mathematics',
-    author: ['K.A. Stroud', 'Dexter J. Booth'],
-    publisher: 'Palgrave',
-    publicationDate: '2007-01-01',
+    title: "Engineering Mathematics",
+    author: ["K.A. Stroud", "Dexter J. Booth"],
+    publisher: "Palgrave",
+    publicationDate: "2007-01-01",
     edition: 14,
-    keywords: ['mathematics', 'engineering'],
+    keywords: ["mathematics", "engineering"],
     pages: 1288,
-    format: 'paperback',
-    ISBN: '9781403942463',
-    language: 'English',
+    format: "paperback",
+    ISBN: "9781403942463",
+    language: "English",
     programmingLanguage: null,
     onlineContent: true,
     thirdParty: {
@@ -115,118 +149,128 @@ const books = [
         ratingsCount: 370,
         reviewsCount: 18,
         fiveStarRatingCount: 211,
-        oneStarRatingCount: 6
-      }
+        oneStarRatingCount: 6,
+      },
     },
-    highlighted: true
+    highlighted: true,
   },
   {
-    title: 'The Personal MBA: Master the Art of Business',
-    author: 'Josh Kaufman',
-    publisher: 'Portfolio',
-    publicationDate: '2010-12-30',
-    keywords: ['business'],
+    title: "The Personal MBA: Master the Art of Business",
+    author: "Josh Kaufman",
+    publisher: "Portfolio",
+    publicationDate: "2010-12-30",
+    keywords: ["business"],
     pages: 416,
-    format: 'hardcover',
-    ISBN: '9781591843528',
-    language: 'English',
+    format: "hardcover",
+    ISBN: "9781591843528",
+    language: "English",
     thirdParty: {
       goodreads: {
         rating: 4.11,
         ratingsCount: 40119,
         reviewsCount: 1351,
         fiveStarRatingCount: 18033,
-        oneStarRatingCount: 1090
-      }
-    }
+        oneStarRatingCount: 1090,
+      },
+    },
   },
   {
-    title: 'Crafting Interpreters',
-    author: 'Robert Nystrom',
-    publisher: 'Genever Benning',
-    publicationDate: '2021-07-28',
-    keywords: ['computer science', 'compilers', 'engineering', 'interpreters', 'software', 'engineering'],
+    title: "Crafting Interpreters",
+    author: "Robert Nystrom",
+    publisher: "Genever Benning",
+    publicationDate: "2021-07-28",
+    keywords: [
+      "computer science",
+      "compilers",
+      "engineering",
+      "interpreters",
+      "software",
+      "engineering",
+    ],
     pages: 865,
-    format: 'paperback',
-    ISBN: '9780990582939',
-    language: 'English',
+    format: "paperback",
+    ISBN: "9780990582939",
+    language: "English",
     thirdParty: {
       goodreads: {
         rating: 4.7,
         ratingsCount: 253,
         reviewsCount: 23,
         fiveStarRatingCount: 193,
-        oneStarRatingCount: 0
-      }
-    }
+        oneStarRatingCount: 0,
+      },
+    },
   },
   {
-    title: 'Deep Work: Rules for Focused Success in a Distracted World',
-    author: 'Cal Newport',
-    publisher: 'Grand Central Publishing',
-    publicationDate: '2016-01-05',
+    title: "Deep Work: Rules for Focused Success in a Distracted World",
+    author: "Cal Newport",
+    publisher: "Grand Central Publishing",
+    publicationDate: "2016-01-05",
     edition: 1,
-    keywords: ['work', 'focus', 'personal development', 'business'],
+    keywords: ["work", "focus", "personal development", "business"],
     pages: 296,
-    format: 'hardcover',
-    ISBN: '9781455586691',
-    language: 'English',
+    format: "hardcover",
+    ISBN: "9781455586691",
+    language: "English",
     thirdParty: {
       goodreads: {
         rating: 4.19,
         ratingsCount: 144584,
         reviewsCount: 11598,
         fiveStarRatingCount: 63405,
-        oneStarRatingCount: 1808
-      }
+        oneStarRatingCount: 1808,
+      },
     },
-    highlighted: true
-  }
+    highlighted: true,
+  },
 ];
 
-const [, , thirdBook] = books
-console.log(thirdBook)
-const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
+const [, , thirdBook] = books;
+console.log(thirdBook);
+const ratings = [
+  ["rating", 4.19],
+  ["ratingsCount", 144584],
+];
 const [[, rating], [, ratingsCount]] = ratings;
-console.log(rating, ratingsCount)
+console.log(rating, ratingsCount);
 const ratingStars = [63405, 1808];
-const [fiveStarRatings, oneStarRatings, threeStarRatings=0] = ratingStars
-console.log(fiveStarRatings, oneStarRatings, threeStarRatings)
+const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
 
 const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
   players: [
     [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
     ],
     [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
     ],
   ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
   odds: {
     team1: 1.33,
     x: 3.25,
@@ -234,14 +278,38 @@ const game = {
   },
 };
 
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+
+const odds = Object.values(game.odds);
+
+let avg = 0;
+
+for (const odd of odds) avg += odd;
+
+console.log(`Average = ${avg / odds.length}`);
+
+for (const [key, value] of Object.entries(game.odds)) {
+  const teamStr = key === "x" ? "draw" : `victory ${game[key]}`;
+  console.log(`Odd of ${teamStr} : ${value}`);
+}
+
+let scorers = new Object();
+
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+
+console.log(`scorers : ${Object.entries(scorers)}`);
 
 const [players1, players2] = game.players;
 console.log(players1, players2);
-const [goalKeeper] = players1
+const [goalKeeper] = players1;
 console.log(goalKeeper);
-const allPlayers = [...players1, ...players2]
+const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+const players1Final = [...players1, "Thiago", "Coutinho", "Periscic"];
 
 const {
   odds: { team1, x: draw, team2 },
@@ -257,7 +325,100 @@ const printGoals = function (...players) {
 // printGoals('Davies', 'Muller');
 printGoals(...game.scored);
 
-team1 < team2 && console.log('Team 1 is more likely to win');
-team1 > team2 && console.log('Team 2 is more likely to win');
+team1 < team2 && console.log("Team 1 is more likely to win");
+team1 > team2 && console.log("Team 2 is more likely to win");
 
+// Coding Challenge #3
 
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+let events = new Array();
+
+for (const event of new Set(gameEvents.values())) {
+  events.push(event);
+}
+
+console.log(events);
+
+gameEvents.delete(64);
+
+console.log(gameEvents);
+
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
